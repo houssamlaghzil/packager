d5e8d701ef7bd7163934ec3f1857d57e49e83e4a
tchydemian <marietchydemian@edu.itescia.fr>
2019-12-03 18:13:02 +0100

    add blurFilter use library

--- packager/packager/src/main/java/com/packager/AppFilter.java
+++ packager/packager/src/main/java/com/packager/AppFilter.java
@@ -3,13 +3,8 @@ package com.packager;
 public class AppFilter {
     public static void main(String[] args) {
 
-
-
         System.out.println("application has started");
 
-
-
-
-
     }
+    
 }
--- packager/packager/src/main/java/com/packager/BlurFilter.java
+++ packager/packager/src/main/java/com/packager/BlurFilter.java
@@ -1,4 +1,25 @@
 package com.packager;
 
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_core.Size;
+
 public class BlurFilter {
+
+    public static void blurFilter(String filename) {
+        Mat image = imread(filename);
+        if (image != null) {
+            GaussianBlur(image, image, new Size(3, 3), 0);
+            imwrite(filename, image);
+        }
+    }
+    private static void GaussianBlur(Mat image, Mat image1, Size size, int i) {
+    }
+
+    private static void imwrite(String filename, Mat image) {
+    }
+
+    private static Mat imread(String filename) {
+        return null;
+    }
+
 }
