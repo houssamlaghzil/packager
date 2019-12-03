34219cd96cf384dbf5a06d66a58152591c88aa24
houssam <hlaghzil@gmail.com>
2019-12-03 22:28:36 +0100

    noir et blanc class complete

--- packager/packager/src/main/java/com/packager/AppFilter.java
+++ packager/packager/src/main/java/com/packager/AppFilter.java
@@ -1,10 +1,45 @@
 package com.packager;
 
-public class AppFilter {
+import java.util.Scanner;
+
+public abstract class AppFilter {
     public static void main(String[] args) {
 
         System.out.println("application has started");
-
+        print("|------------ MENU ------------|");
+        print("|         0 - blur             |");
+        print("|         1 - BnWfilter        |");
+        print("|         2 - DikateFilter     |");
+        print("|         3 - exite            |");
+        print("|------------------------------|");
     }
-    
+
+    public static void print(Object o)     /** print shortcut method */
+    {
+        System.out.println(o);
+    }
+
+
+
+    public static void choiceFilter() {
+        Scanner sc = new Scanner(System.in);
+        String filter = sc.nextLine();
+
+        while (!filter.equals("3")) {
+            if (filter.equals("0")) {
+                print("blur effct");
+
+
+            } else if (filter.equals("1")) {
+                print("BnWFilter");
+                new BnWFilter();
+
+            } else if (filter.equals("2")) {
+                print("DilateFilter");
+
+
+            }
+        }
+        print("okey bye");
+    }
 }
--- packager/packager/src/main/java/com/packager/BnWFilter.java
+++ packager/packager/src/main/java/com/packager/BnWFilter.java
@@ -1,4 +1,49 @@
 package com.packager;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+import static javax.imageio.ImageIO.read;
+
+/**
+ * Convertir une image en nuance de gris
+ * @author Axel - ® fobec.com 2010
+ */
 public class BnWFilter {
+
+    public static void main(String args[]) {
+        try {
+            System.out.println("Début de conversion....");
+            //Ouverture du fichier
+            /**
+             * Image d'entrée en couleur
+             */
+            String inFilename = "file:///Users/souksou/Documents/img/8326519c9fbbbb4.png";
+            File inputFile = new File(inFilename);
+            BufferedImage imagesrc;
+            imagesrc = read(inputFile);
+            //Convertion en grisé
+            BufferedImage imagedst = new BufferedImage(imagesrc.getWidth(),
+
+                    imagesrc.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+            Graphics g = imagedst.getGraphics();
+            g.drawImage(imagesrc, 0, 0, null);
+            g.dispose();
+            //Enregistrer l'image au format PNG
+            /**
+             * Image de sortie en nuance de gris
+             */
+            String outFilename = "file:///Users/souksou/Documents/img/8326519c9fbbbb4.png";
+            File outFile = new File(outFilename);
+            ImageIO.write(imagedst, "PNG", outFile);
+            System.out.println("Fin de conversion....");
+        } catch (IOException ex) {
+            Logger.getLogger(BnWFilter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
