a30665116bcbe22633ab609ececc311a5a340d9a
Unknown <hart.francesco@gmail.com>
2019-12-04 13:54:02 +0100

    Filters

--- packager/packager/src/main/java/com/packager/BlurFilter.java
+++ packager/packager/src/main/java/com/packager/BlurFilter.java
@@ -1,19 +1,23 @@
 package com.packager;
 
 import org.bytedeco.opencv.opencv_core.*;
-
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
-
 import org.bytedeco.opencv.opencv_core.Size;
 
 
-public class BlurFilter {
+public class BlurFilter implements IFilter{
 
-    public Mat filterBlur(Mat image) {
+
+
+    @Override
+    public void filter(String fileIn, String fileOut) {
+        Mat img = imread(fileIn);
         int size = 3;
-        Mat result = image.clone();
-        GaussianBlur(image, result, new Size(size, size), 0);
-        return result;
-    }
+        Mat result = img.clone();
+        GaussianBlur(img, result, new Size(size, size), 0);
+        imwrite(fileOut, img);
 
+    }
 }
--- packager/packager/src/main/java/com/packager/DilateFilter.java
+++ packager/packager/src/main/java/com/packager/DilateFilter.java
@@ -1,23 +1,23 @@
 package com.packager;
 
 
-import org.bytedeco.opencv.opencv_core.Size;
-import org.opencv.core.Mat;
+import org.bytedeco.opencv.opencv_core.*;
 import org.opencv.imgproc.Imgproc;
-import static org.opencv.imgproc.Imgproc.dilate;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
 
-public class DilateFilter{
 
-    public Mat filterDilate(Mat image) {
+
+public class DilateFilter implements IFilter{
+
+    @Override
+    public void filter(String fileIn, String fileOut) {
+        Mat img = imread(fileIn);
         int size = 8;
-        Mat result = image.clone();
+        Mat result = img.clone();
         Mat element = getStructuringElement(Imgproc.MORPH_RECT, new Size(2 * size + 1, 2 * size + 1));
-        dilate(image, result, element);
-        return result;
-    }
-
-    private Mat getStructuringElement(int morphRect, Size size) {
-        return null;
+        dilate(img, result, element);
+        imwrite(fileOut, img);
     }
 }
 
