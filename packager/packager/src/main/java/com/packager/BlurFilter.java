28f2583577f4a95be7f4313078601d028a0a4026
Unknown <hart.francesco@gmail.com>
2019-12-04 09:09:20 +0100

    blure and import file

--- packager/packager/src/main/java/com/packager/BlurFilter.java
+++ packager/packager/src/main/java/com/packager/BlurFilter.java
@@ -1,4 +1,15 @@
 package com.packager;
 
+import org.bytedeco.opencv.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
+
 public class BlurFilter {
+    public static void smooth(String filename) {
+        Mat image = imread(filename);
+        if (image != null) {
+            GaussianBlur(image, image, new Size(3, 3), 0);
+            imwrite(filename, image);
+        }
+    }
 }
