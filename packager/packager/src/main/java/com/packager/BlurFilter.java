a9bb6bd17f4338eb610e4a7bd61e9de18af3b554
tchydemian <marietchydemian@edu.itescia.fr>
2019-12-04 10:08:10 +0100

    Auto stash before merge of "master" and "origin/master"

--- packager/packager/src/main/java/com/packager/BlurFilter.java
+++ packager/packager/src/main/java/com/packager/BlurFilter.java
@@ -42,5 +42,20 @@ public class BlurFilter<image> {
          * flou
          * @param filename
          */
+<<<<<<< Updated upstream
+=======
+            Mat image = imread("/Users/tchydemianmarie/Desktop/packager/packager/packager/src/main/java/com/packager/imgin");//Open file OpenCV format
+            if (image != null) {
+                GaussianBlur(image, image, new Size(3, 3), 0);//flou img
+                imwrite("/Users/tchydemianmarie/Desktop/packager/packager/packager/src/main/java/imgout", image);//save img flou
+            }
+
+
+        }
+
+
+
+
+>>>>>>> Stashed changes
     }
 }
