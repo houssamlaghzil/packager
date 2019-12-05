73c94ba7005c2becc1f1aebe330b4369aadba81c
Unknown <hart.francesco@gmail.com>
2019-12-05 09:43:49 +0100

    option write

--- packager/packager/src/main/java/com/packager/AppFilter.java
+++ packager/packager/src/main/java/com/packager/AppFilter.java
@@ -1,6 +1,6 @@
 package com.packager;
 
-
+import org.apache.commons.cli.*;
 import org.bytedeco.opencv.opencv_core.Mat;
 
 import java.io.File;
@@ -14,22 +14,46 @@ import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 public abstract class AppFilter {
     public static void main(String[] args) {
 
+        CommandLineParser parser = new DefaultParser();
+
+        // create the Options
+        Options options = new Options();
+        
+        options.addOption( "h", "help", false, "" );
+        options.addOption( "i", "input-dir", true, "" );
+        options.addOption( "o", "output-dir", true, "" );
+
 
         List<IFilter> filterArray = new ArrayList<>(); // liste des filtres a applliqué
 
-
+        System.out.println(args.length+" args" );
         String filterArg = "ce qui est en ligne de commande";
         String[] split = filterArg.split("\\|");
 
         for (String s : split){
-            switch (s){
+            // s = blur:3
+            // s = grayscale
+            String[] split2 = s.split("\\:");
+            // blur, 3 ==> size=2
+            // grayscale ==> size=1
+
+            String param = "";
+            if(split2.length == 2){
+                param = split2[1];
+            }
+
+            //integer.parsint(a) int
+            switch (split2[0]){
                 case "blur":
+                    int i = Integer.parseInt(param);
                     filterArray.add(new BlurFilter());
                     break;
-                case  "grayscale":
+                case  "bnw":
+
                     filterArray.add(new BnWFilter());
                     break;
                 case  "dilate":
+                    int j = Integer.parseInt(param);
                     filterArray.add(new DilateFilter());
                     break;
             }
